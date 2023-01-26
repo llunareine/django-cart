7c6d3995c6529165cc7ccc64cf0a5dd55502d2ad
llunareine <llunareine@gmail.com>
2023-01-26 23:14:39 +0600

    refactor:change from class apiview to generics view

--- api/urls.py
+++ api/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('users', views.Users.as_view(), name='users'),
+    path('users', views.UserList.as_view(), name='users'),
     path('users/<int:user_id>', views.UserDetail.as_view(), name='user_id')
 ]
